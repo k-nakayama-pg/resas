4bfceee23d90f24d124a0e541ae849871f8687bd
k-nakayama-pg <k.nakayama.pg@gmail.com>
2017-11-19 11:49:33 +0900

    test

--- app.js
+++ app.js
@@ -6,310 +6,130 @@ var crypto = require("crypto");
 var async = require('async');
 var fs = require('fs');
 
-global.nakayama_kazuya_line_id = 'U2aca57e8b1a096a56b1199ae5311f7e5';
-global.nakamura_shigeki_line_id = 'U4002d7027f26356aba3fecbb0e00f369';
-
 app.set('port', (process.env.PORT || 8000));
 // JSONの送信を許可
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
 // JSONのパースを楽に（受信時）
 app.use(bodyParser.json());
 
 app.post('/callback', function(req, res) {
-  async.waterfall([
-      function(callback) {
-
-        console.log(req.body);
-        console.log(req.body['events'][0]['message']);
-
-        // リクエストがLINE Platformから送られてきたか確認する
-        if (!validate_signature(req.headers['x-line-signature'], req.body)) {
-          return;
+    async.waterfall([
+            function(callback) {
+                // リクエストがLINE Platformから送られてきたか確認する
+                if (!validate_signature(req.headers['x-line-signature'], req.body)) {
+                    return;
+                }
+                // テキストが送られてきた場合のみ返事をする
+                if ((req.body['events'][0]['type'] != 'message') || (req.body['events'][0]['message']['type'] != 'text')) {
+                    return;
+                }
+                // 「くっころ」または「絶唱顔」という単語がテキストに含まれている場合のみ返事をする
+                if (req.body['events'][0]['message']['text'].indexOf('くっころ') != -1) {
+                    // 1対1のチャットの場合は相手のユーザ名で返事をする
+                    // グループチャットの場合はユーザ名が分からないので、「貴様ら」で返事をする
+                    if (req.body['events'][0]['source']['type'] == 'user') {
+                        // ユーザIDでLINEのプロファイルを検索して、ユーザ名を取得する
+                        var user_id = req.body['events'][0]['source']['userId'];
+                        var get_profile_options = {
+                            url: 'https://api.line.me/v2/bot/profile/' + user_id,
+                            proxy: process.env.FIXIE_URL,
+                            json: true,
+                            headers: {
+                                'Authorization': 'Bearer {' + process.env.LINE_CHANNEL_ACCESS_TOKEN + '}'
+                            }
+                        };
+                        request.get(get_profile_options, function(error, response, body) {
+                            if (!error && response.statusCode == 200) {
+                                callback(create_text_options(body['displayName'] + 'にこんな辱めを受けるとは...！\nくっ...殺せ！', req.body));
+                            }
+                        });
+                    } else if ('room' == req.body['events'][0]['source']['type']) {
+                        callback(create_text_options('貴様ら' + 'にこんな辱めを受けるとは...！\nくっ...殺せ！', req.body));
+                    }
+                } else if (req.body['events'][0]['message']['text'].indexOf('絶唱顔') != -1) {
+                    callback(create_img_options('https://kukkoro-bot.herokuapp.com/img/zessyougao.jpg', req.body));
+                } else {
+                    return;
+                }
+            },
+        ],
+        function(options) {
+            request.post(options, function(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log(body);
+                } else {
+                    console.log('error: ' + JSON.stringify(response));
+                }
+            });
         }
-        // テキストが送られてきた場合のみ返事をする
-        if (!(req.body['events'][0]['type'] == 'message' || req.body['events'][0]['type'] == 'beacon')) {
-          return;
-        }
-
-        if (req.body['events'][0]['type'] == 'message' && req.body['events'][0]['message']['text'].indexOf('助けてほしい人_ビーコン_オン') != -1) {
-          console.log('===== 助けてほしい人_ビーコン_オンと入力されましたa =====');
-          request.post(create_push_help_message(global.nakayama_kazuya_line_id, "近くに困っている人がいます。"), function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-              console.log(body);
-            } else {
-              console.log('error: ' + JSON.stringify(response));
-            }
-          });
-        } else if (req.body['events'][0]['type'] == 'message' && req.body['events'][0]['message']['text'].indexOf('助ける_ビーコン_オン') != -1) {
-          console.log('===== 助ける_ビーコン_オンと入力されました =====');
-          request.post(create_push_tasukeru_message(global.nakamura_shigeki_line_id, "近くに 英語で道案内 についてサポートできる人が見つかりました。"), function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-              console.log(body);
-            } else {
-              console.log('error: ' + JSON.stringify(response));
-            }
-          });
-        } else if (req.body['events'][0]['type'] == 'beacon') {
-          console.log('===== 助けてほしい人が実機のビーコンをオンにしました =====');
-          request.post(create_push_help_message(req.body['events'][0]["source"]["userId"], "近くに困っている人がいます。"), function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-              console.log(body);
-            } else {
-              console.log('error: ' + JSON.stringify(response));
-            }
-          });
-        } else if (req.body['events'][0]['type'] == 'message' && req.body['events'][0]['message']['text'].indexOf('任せて！') != -1) {
-          console.log('===== はい、助けます。と入力されました =====');
-          request.post(create_push_can_help_message(global.nakamura_shigeki_line_id, "近くでサポートしてくれる人がみつかりました！"), function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-              console.log(body);
-            } else {
-              console.log('error: ' + JSON.stringify(response));
-            }
-          });
-        } else if (req.body['events'][0]['type'] == 'message' && req.body['events'][0]['postback'] != null) {
-          console.log('===== 助けてほしい人に選ばれました =====');
-          request.post(create_push_can_help_location_message("中山一哉", ""), function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-              console.log(body);
-            } else {
-              console.log('error: ' + JSON.stringify(response));
-            }
-          });
-        }
-      },
-    ],
-    function(options) {
-      request.post(options, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-          console.log(body);
-        } else {
-          console.log('error: ' + JSON.stringify(response));
-        }
-      });
-    }
-  );
+    );
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running');
+    console.log('Node app is running');
+});
+
+app.get('/img/zessyougao.jpg', function(req, res) {
+    fs.readFile('img/zessyougao.jpg', function(err, data) {
+        res.set('Content-Type', 'image/jpeg');
+        res.send(data);
+    });
 });
 
 // 署名検証
 function validate_signature(signature, body) {
-  return signature == crypto.createHmac('sha256', process.env.LINE_CHANNEL_SECRET).update(new Buffer(JSON.stringify(body), 'utf8')).digest('base64');
+    return signature == crypto.createHmac('sha256', process.env.LINE_CHANNEL_SECRET).update(new Buffer(JSON.stringify(body), 'utf8')).digest('base64');
 }
 
-// LINEの友達に助けてほしいメッセージリクエストを作成
-function create_push_help_message(user_id, text) {
-  //ヘッダーを定義
-  var headers = {
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer {' + process.env.LINE_CHANNEL_ACCESS_TOKEN + '}',
-  };
-
-  // 送信データ作成
-  var data;
-  if (text != null) {
-    data = {
-      'to': user_id,
-      "messages": [{
-        'type': "template",
-        "altText": "this is a buttons template",
-        "template": {
-          "type": "confirm",
-          "text": text,
-          "actions": [{
-              "type": "message",
-              "label": "任せて！",
-              "text": "任せて！"
-            },
-            {
-              "type": "message",
-              "label": "今無理。。。",
-              "text": "今無理。。。"
-            }
-          ]
-        }
-      }]
-    };
-  }
-
-  //オプションを定義
-  var options = {
-    url: 'https://api.line.me/v2/bot/message/push',
-    headers: headers,
-    json: true,
-    body: data
-  };
-
-  console.log('===== options =====\n' + options);
-  return options;
+// LINEに送るテキストリクエストを作成
+function create_text_options(text, body) {
+    return create_options(text, null, body);
 }
 
-// LINEの友達に助けてほしいメッセージリクエストを作成
-function create_push_tasukeru_message(user_id, text) {
-  //ヘッダーを定義
-  var headers = {
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer {' + process.env.LINE_CHANNEL_ACCESS_TOKEN + '}',
-  };
-
-  // 送信データ作成
-  var data;
-  if (text != null) {
-    data = {
-      'to': user_id,
-      "messages": [{
-        'type': "template",
-        "altText": "this is a buttons template",
-        "template": {
-          "type": "confirm",
-          "text": text,
-          "actions": [{
-              "type": "message",
-              "label": "助けてー",
-              "text": "助けてー"
-            },
-            {
-              "type": "message",
-              "label": "大丈夫！",
-              "text": "大丈夫！"
-            }
-          ]
-        }
-      }]
-    };
-  }
-
-  //オプションを定義
-  var options = {
-    url: 'https://api.line.me/v2/bot/message/push',
-    headers: headers,
-    json: true,
-    body: data
-  };
-
-  console.log('===== options =====\n' + options);
-  return options;
-}
-
-// LINEの助けてくれる友達リストのメッセージリクエストを作成
-function create_push_can_help_message(user_id, text) {
-  //ヘッダーを定義
-  var headers = {
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer {' + process.env.LINE_CHANNEL_ACCESS_TOKEN + '}',
-  };
-
-  // 送信データ作成
-  var data;
-  if (text != null) {
-    data = {
-      'to': user_id,
-      "messages": [{
-          'type': "text",
-          'text': text
-        },
-        {
-          'type': "template",
-          "altText": "this is a buttons template",
-          "template": {
-            "type": "buttons",
-            "text": "近くでサポートしてくれる人",
-            "actions": [{
-                "type": "postback",
-                "label": "中山一哉さん, 男性, 25歳",
-                "data": "help_user_id=nakayama"
-              },
-              {
-                "type": "postback",
-                "label": "美樹子さん, 女性",
-                "data": "help_user_id=mikiko"
-              }
-            ]
-          }
-        }
-      ]
-    };
-  }
-
-  //オプションを定義
-  var options = {
-    url: 'https://api.line.me/v2/bot/message/push',
-    headers: headers,
-    json: true,
-    body: data
-  };
-
-  console.log('===== options =====\n' + options);
-  return options;
-}
-
-// LINEの助けてくれる友達リストのメッセージリクエストを作成
-function create_push_can_help_location_message(user_id, text) {
-  //ヘッダーを定義
-  var headers = {
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer {' + process.env.LINE_CHANNEL_ACCESS_TOKEN + '}',
-  };
-
-  // 送信データ作成
-  var data;
-  if (text != null) {
-    data = {
-      'to': global.nakamura_shigeki_line_id,
-      "messages": [{
-        'type': "location",
-        "title": user_id + "さんの位置情報",
-        "address": "〒261-0014 千葉県千葉市美浜区若葉3丁目１－２１ 幕張neighborhoodPOD",
-        "latitude": 35.647885,
-        "longitude": 140.046072
-      }]
-    };
-  }
-
-  //オプションを定義
-  var options = {
-    url: 'https://api.line.me/v2/bot/message/push',
-    headers: headers,
-    json: true,
-    body: data
-  };
-
-  console.log('===== options =====\n' + options);
-  return options;
+// LINEに送るイメージリクエストを作成
+function create_img_options(img, body) {
+    return create_options(null, img, body);
 }
 
 // LINEに送るリクエストを作成
-function create_options(text, body) {
-  //ヘッダーを定義
-  var headers = {
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer {' + process.env.LINE_CHANNEL_ACCESS_TOKEN + '}',
-  };
-
-  // 送信データ作成
-  var data;
-  if (text != null) {
-    data = {
-      'replyToken': body['events'][0]['replyToken'],
-      "messages": [{
-        "type": "text",
-        "text": text
-      }]
+function create_options(text, img, body) {
+    //ヘッダーを定義
+    var headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer {' + process.env.LINE_CHANNEL_ACCESS_TOKEN + '}',
     };
-  }
 
-  //オプションを定義
-  var options = {
-    url: 'https://api.line.me/v2/bot/message/reply',
-    headers: headers,
-    json: true,
-    body: data
-  };
+    // 送信データ作成
+    var data;
+    if (text != null) {
+        data = {
+            'replyToken': body['events'][0]['replyToken'],
+            "messages": [{
+                "type": "text",
+                "text": text
+            }]
+        };
+    } else if (img != null) {
+        data = {
+            'replyToken': body['events'][0]['replyToken'],
+            "messages": [{
+                "type": "image",
+                "originalContentUrl": img,
+                "previewImageUrl": img
+            }]
+        };
+    }
 
-  return options;
+    //オプションを定義
+    var options = {
+        url: 'https://api.line.me/v2/bot/message/reply',
+        proxy: process.env.FIXIE_URL,
+        headers: headers,
+        json: true,
+        body: data
+    };
+
+    return options;
 }
